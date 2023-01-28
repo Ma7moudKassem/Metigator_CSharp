14d8330dc1371ddd71214598a314c687a03e1112
Mahmoud Kassem <modykassem123@gmail.com>
2023-01-28 19:19:30 +0200

    Finalizer

---
+++ Finalizer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine($"Memory usage before MakeSomeCarbadge: {GC.GetTotalMemory(false):N0}");
+MakeSomeCarbadge();
+Console.WriteLine($"Memory usage before collect: {GC.GetTotalMemory(false):N0}");
+GC.Collect();
+Console.WriteLine($"Memory usage after collect: {GC.GetTotalMemory(false):N0}");
+
+
+void MakeSomeCarbadge()
+{
+    Version v;
+    for (int i = 0; i <= 1000; i++)
+        v = new();
+}
