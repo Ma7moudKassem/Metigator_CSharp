19c621bca50c74b2b5d65b9bb11e100ed946f1ea
Mahmoud Kassem <modykassem123@gmail.com>
2023-02-05 03:12:17 +0200

    commit

---
+++ Enumerators and  Iterators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+FiveIntegersNumbers fiveIntegers = new(1, 2, 3, 4, 5);
+foreach (int item in fiveIntegers)
+    Console.WriteLine(item);
+
+
+
+public class FiveIntegersNumbers : IEnumerable
+{
+    int[] values;
+    public FiveIntegersNumbers(int n1, int n2, int n3, int n4, int n5)
+    {
+        values = new[] { n1, n2, n3, n4, n5 };
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (var item in values)
+            yield return item;
+    }
+}
