6d19b548ee0d2dbc0bf9fc0f3d323eccd9f66624
Mahmoud Kassem <modykassem123@gmail.com>
2023-02-08 00:10:54 +0200

    commit

---
+++ Assemblies/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+Type type = typeof(Program);
+Assembly assembly = type.Assembly;
+
+Stream? streem = assembly.GetManifestResourceStream(type, "Phones.json");
+
+if (streem is null) return;
+byte[] data = new BinaryReader(streem).ReadBytes((int)streem.Length);
+
+foreach (byte da in data)
+{
+    Console.WriteLine((char)da);
+    System.Threading.Thread.Sleep(300);
+}
+streem.Close()
---
+++ Operator Overloading/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Operator_Overloading
+{
+    public class Demo
+    {
+        public static void PrintAssemblies()
+        {
+            Console.WriteLine($"Executing assemblies {Assembly.GetExecutingAssembly()}");
+            Console.WriteLine($"CallingAssembly assemblies {Assembly.GetCallingAssembly()}");
+            Console.WriteLine($"EntryAssembly assemblies {Assembly.GetEntryAssembly()}");
+        }
+    }
+}
