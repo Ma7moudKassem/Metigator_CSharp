6f25ec193becf9195a76743cd375a961f5db57ef
Mahmoud Kassem <modykassem123@gmail.com>
2023-02-14 00:03:25 +0200

    commit

---
+++ Core Of LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿//List<int> nums = new() { 1, 2, 3, 4, 5, 6 };
+
+//var result = from n in nums
+//             where n % 2 == 0
+//             select n;
+
+//result.ToList().ForEach(e => { Console.WriteLine(e); });
+
+using System.Linq;
+
+List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+IEnumerable<int> evenNumbers = numbers.Where(x => x % 2 == 0);
+//IEnumerable<int> evenNumbers = Enumerable.Where(numbers, x => x % 2 == 0);
+
+numbers.Add(10);
+numbers.Add(12);
+numbers.Remove(4);
+
+foreach (int number in evenNumbers)
+    Console.WriteLine(number);
