890384b1fba0333a5d8f3cf96047d8ef76b1e145
Mahmoud Kassem <modykassem123@gmail.com>
2023-02-16 02:16:31 +0200

    commit

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,44 @@
+﻿//var article = "Dot NET is a free cross-platform and open source developer platform" +
+//              "for building many different types of applications" +
+//              "With Dot NET you can use multiple languages and libraries" +
+//              "to build for web and IoT";
+
+//Dictionary<char, List<string>> map = new();
+
+//foreach (string word in article.Split())
+//{
+//    foreach (char ch in word)
+//        if (!map.ContainsKey(ch))
+//            map.Add(ch, new List<string> { word });
+//        else
+//            map[ch].Add(word);
+//}
+
+//Console.WriteLine();
+
+List<Employee> employees = new()
+{
+    new Employee{Id=1 , Name="Mahmoud",ReportTo=100 },
+    new Employee{Id=2 , Name="Kassem",ReportTo=150 },
+    new Employee{Id=3 , Name="Ahmed",ReportTo=200 },
+    new Employee{Id=4 , Name="Hussien",ReportTo=500 },
+    new Employee{Id=5 , Name="Mohamed",ReportTo=null },
+};
+
+var managers = employees.GroupBy(e => e.ReportTo).ToDictionary(e => e.Key ?? -1, e => e.ToList());
+var managersToLookUp = employees.ToLookup(e => e.ReportTo).ToDictionary(e => e.Key ?? -1, e => e.ToList());
+
+foreach (var manager in managers)
+{
+    if (manager.Key == -1) continue;
+    Console.Write($"{manager.Key}");
+    manager.Value.ForEach(e => { Console.WriteLine($"\t\t{e.Name}"); });
+}
+
+Console.WriteLine();
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ReportTo { get; set; }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Country egypt = new() { Id = 1, Name = "Egypt" };
+Country qatar = new() { Id = 2, Name = "Qatar" };
+Country yamn = new() { Id = 3, Name = "Yamn" };
+
+Country[] countries = new[]
+{
+    yamn,
+    egypt,
+    qatar,
+};
+List<Country> countriesList = new();
+
+countriesList.AddRange(countries);
+
+Console.WriteLine($"List count = {countriesList.Count}");
+countriesList.Add(new());
+countriesList.Remove(countriesList[countriesList.IndexOf(yamn)]);
+countriesList.Remove(countriesList[countriesList.IndexOf(new() { Id = 2, Name = "Qatar" })]);
+Console.WriteLine($"List capacity = {countriesList.Capacity}");
+Console.WriteLine($"List count agine = {countriesList.Count}");
+
+foreach (var country in countriesList)
+    Console.WriteLine(country.ToString());
+class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id : {Id}\nName : {Name}";
+    }
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        hash = hash * 701 + Name.GetHashCode();
+        hash = hash * 701 + Id.GetHashCode();
+
+        return hash;
+    }
+    public override bool Equals(object? obj)
+    {
+        var country = obj as Country;
+        if (country is null) return false;
+
+
+        return this.Name.Equals(country.Name, StringComparison.OrdinalIgnoreCase);
+    }
+}
